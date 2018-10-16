d83211c469f80e683c75fb543de58081d2465add
Exo-poulpe <33369156+Exo-poulpe@users.noreply.github.com>
2018-10-16 10:49:19 +0200

    Add bootstrap to project

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date dans le passé
 
   <title>Bouton</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
 
   <script>var ADDR = JSON.parse('<?= json_encode(ADDR) ?>');</script>
   <script src="./javascript.js"></script>
@@ -71,20 +73,19 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date dans le passé
   <tr>
     <?php
     for ($i=0; $i < 2; $i++) {if ($i == 1) {
-      print('</tr></tabl><table border="solid" id="TableHex2" class="Table"><tr>');
+      print('</tr></table><table border="solid" id="TableHex2" class="Table"><tr>');
     } ?>
     <td>
       <?php print('<div id="hex'.$i.'">0</div>'); ?>
     </td>
   <?php } ?>
-</tr>
+    </tr>
 </table>
-
-
+  
 <form method="post" action="serveur.php">
-  <input type="Button" value="0" name="Button" onclick="ListSetter(0)" id="B0" class="Button"/>
-  <input type="Button" value="1" name="Button" onclick="ListSetter(1)" id="B1" class="Button"/>
-  <input type="Button" value="X" name="Delete" onclick="ResetArray()" id="X" class="Button"/>
+  <input type="Button" value="0" name="Button" onclick="ListSetter(0)" id="B0" class="Button btn btn-secondary"/>
+  <input type="Button" value="1" name="Button" onclick="ListSetter(1)" id="B1" class="Button btn btn-secondary"/>
+  <input type="Button" value="X" name="Delete" onclick="ResetArray()" id="X" class="Button btn btn-secondary"/>
 </form>
 
 </body>
